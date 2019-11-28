6092bf0c7d3e11e6abe3d24f1084a778cccd4e25
raheelarif <raheelarif>
2019-11-28 17:03:16 +0500

    css

--- src/components/home/section1/home-right/chatComponent.js
+++ src/components/home/section1/home-right/chatComponent.js
@@ -51,7 +51,7 @@ class Chat extends React.Component {
                     // className={item.user._id !== "234k3jjk" ?"message":"my_message"}
                     >
                         <div className={`message-content${item.index}`}>
-                            <p style={{ fontSize: "12px", paddingBottom: "20px" }}> {item.message} </p>
+                            <p style={{ fontSize: "12px", paddingBottom: "20px" }}> {item.message}</p>
                         </div>
 
                     </div>
@@ -86,21 +86,16 @@ class Chat extends React.Component {
                 <div className="container">
                     <div className="row">
                         <div className="col col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12"
-                            style={{ display: "flex",border:"1px solid black", flexDirection: "column", justifyItems: "center",  }}
+                            style={{ display: "flex", flexDirection: "column", justifyItems: "center",  }}
                         >
                             <div>
-                                <div
-                                    style={{
-                                        height: "278px", backgroundColor: "white", display: "flex", flexDirection: "column", alignItems: "center", width: "97%",
-                                        marginLeft: "3px"
-                                    }}
-                                >
+                                <div className="mesg-area-r">
                                     <div
                                         className="mainchatbox"
                                     >
                                         <List
                                             width={999}
-                                            height={260}
+                                            height={230}
                                             rowCount={chat.messages.length}
                                             rowHeight={({ index }) => { return chat.messages[index].message.length * 4 + 20 }}
                                             style={{ width: "100%", marginTop: "10px", outline: "none" }}
