eb56647d0ff762301ee2f7c1d9367574c888312d
raheelarif <raheelarif>
2019-12-19 12:22:39 +0500

    slider

--- src/components/home/section4a/section4.js
+++ src/components/home/section4a/section4.js
@@ -4,7 +4,7 @@ import Slider from './slider'
 export default class Section1 extends Component {
     render() {
         return (
-            <div className="section4-ca" id="section3">
+            <div className="section4-c" id="section3">
                  <p className="s4bg">OUR <span>PARTNERS</span></p>
                  <h1 className="s5-h1">In Partnership With</h1>
                 <Slider />
